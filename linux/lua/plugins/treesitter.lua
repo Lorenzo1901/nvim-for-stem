59ec026927c715e6d5722c26f57c2fb4e84fd526
Lorenzo1901 <zambardilorenzo@gmail.com>
2026-05-08 11:33:00 +0200

    sync: 08-05-2026 11:33

--- linux/lua/plugins/treesitter.lua
+++ linux/lua/plugins/treesitter.lua
@@ -8,16 +8,21 @@ return {
     lazy = false,
     build = ':TSUpdate',
     config = function()
+      vim.treesitter.language.register("latex", { "tex" })
+
       require('nvim-treesitter').install({ "lua", "python", "javascript", "html", "css", "latex" })
 
       vim.api.nvim_create_autocmd('FileType', {
         pattern = { "lua", "python", "javascript", "html", "css", "tex" },
-        callback = function()
-          vim.treesitter.start()
-          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        callback = function(ev)
+          pcall(vim.treesitter.start, ev.buf)
+          vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          
+          if ev.match == "tex" then
+            vim.bo[ev.buf].syntax = "tex"
+          end
         end,
       })
     end
   },
 }
-
